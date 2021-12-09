281a180a40bc52a10eae2ec044d14eabd678c67c
Isaac Valadez <angeliss1082@gmail.com>
2021-12-08 19:29:22 -0600

    Issue #3: Type error: Binding element 'Component' implicitly has an 'any' type. #3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/dist/shared/lib/router/router'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
